ab65812233e31b629c658211226d57b1be1eeeea
josemari <jomaveger@gmail.com>
2020-12-12 20:58:58 +0100

    updates

---
+++ BookChaptersCode/src/main/java/org/jomaveger/examples/chapter0/DoWhile.java
@@ -0,0 +1,11 @@
+package org.jomaveger.examples.chapter0;
+
+public class DoWhile {
+	public static void main(String args[]) {
+		int n = 10;
+		do {
+			System.out.println("tick " + n);
+			n--;
+		} while (n > 0);
+	}
+}
---
+++ BookChaptersCode/src/main/java/org/jomaveger/examples/chapter0/FirstExample.java
@@ -0,0 +1,8 @@
+package org.jomaveger.examples.chapter0;
+
+public class FirstExample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
